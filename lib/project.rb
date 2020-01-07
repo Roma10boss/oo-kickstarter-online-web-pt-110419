d1768c2e9768d2b3130f6f0f8b5a06c305b5482f
Romario Gustave <romagustave10@gmail.com>
2020-01-07 12:01:38 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,21 +1,4 @@
 class Project
-  
-  attr_accessor :title, :bakers 
-  
-  def initialize(title)
-    @title = title
-    @bakers = []
-  end 
-  
-  def add_baker(baker)
-    @bakers << baker 
-    baker.backed_projects << self 
-  end 
-  
-end 
-
-
-class Project
   attr_accessor :title, :backers
 
   def initialize(title)
